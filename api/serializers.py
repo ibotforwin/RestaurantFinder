97ef81cb475cfcc337a8477da97b2c86750c7c4d
klimc <ibotforwin@gmail.com>
2020-03-16 16:09:39 -0400

    Clean up whitespace.

--- api/serializers.py
+++ api/serializers.py
@@ -1,18 +1,15 @@
 from rest_framework import serializers
 from .models import Restaurant, UserData, Location
 
-
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = ('id', 'name','latitude','longitude', 'rating')
 
-
 class UserDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserData
         fields = ('id', 'user', 'restaurant', 'blacklisted', 'favorite')
-
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
